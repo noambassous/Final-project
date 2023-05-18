d712e66e338d2c5b52fe411710198abcc426a391
noambassous <127144907+noambassous@users.noreply.github.com>
2023-05-18 20:31:22 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ window.onload = () => {
     var navbar = document.getElementById("navbar");
     var sticky = navbar.offsetTop;
 
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function addStickyClass() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
